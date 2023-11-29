079c1206452a468df0c2defc114975e8819c0b26
TalyaNeppe <talyaneppe@hilma.tech>
2023-11-29 14:28:42 +0200

    something

--- express-server/routes/index.js
+++ express-server/routes/index.js
@@ -8,7 +8,11 @@ router.get("/", function (req, res, next) {
 
 router.get("/:username", function (req, res, next) {
   const user = req.params.username;
-  res.send(req.params.username);
+  res.send(JSON.stringify(req.params.username));
 });
 
 module.exports = router;
+//   \_(o - o)_/
+//jkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjkjk
+//l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.l.
+//jujujujujujujujujujujujujujujujujujujujujujujujujujujujujujujujujujujujujujujujujujuj
