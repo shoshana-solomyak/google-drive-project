4f5e3dec3ceaedee96a94d70a5b0f0ffe8d1acff
TalyaNeppe <talyaneppe@hilma.tech>
2023-11-30 03:51:40 +0200

    show info

--- react/src/components/File.jsx
+++ react/src/components/File.jsx
@@ -1,11 +1,17 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function File({ item, handleDelete }) {
   const navigate = useNavigate();
   const splitName = item.name.split(".");
+  const [showInfo, setShowInfo] = useState(false);
 
   function openFile() {
     navigate(`./file/${splitName[0]}?type=${splitName[1]}`);
+  }
+
+  function handleInfo() {
+    setShowInfo((info) => !info);
   }
 
   return (
@@ -13,13 +19,18 @@ function File({ item, handleDelete }) {
       {item.name}
 
       <button onClick={openFile}>open</button>
-      <button>info</button>
+      <button onClick={handleInfo}>info</button>
       <button
         onClick={() => handleDelete({ itemName: item.name, isFolder: false })}
       >
         🗑️
       </button>
       <button>✏️</button>
+      {showInfo ? (
+        <span>
+          size: {item.size}, birthday: {item.birthday}
+        </span>
+      ) : null}
     </div>
   );
 }
